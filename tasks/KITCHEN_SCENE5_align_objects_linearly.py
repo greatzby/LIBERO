03e1949ff79adcd516138aae63ddefe0e1efca35
ArcaLunar <arca@connect.hku.hk>
2025-06-06 07:59:24 +0800

    fix inair hack

--- tasks/KITCHEN_SCENE5_align_objects_linearly.py
+++ tasks/KITCHEN_SCENE5_align_objects_linearly.py
@@ -13,7 +13,10 @@ def main():
         objects_of_interest=["plate_1", "ketchup_1", "akita_black_bowl_1"],
         goal_states=[
             ("linear", "plate_1", "ketchup_1", "akita_black_bowl_1", 0.005),
-            ("ordering", "plate_1", "ketchup_1", "akita_black_bowl_1")
+            ("ordering", "plate_1", "ketchup_1", "akita_black_bowl_1"),
+            ("Not", ("inair", "plate_1", 0.903)),  # initial state: z=0.9025
+            ("Not", ("inair", "ketchup_1", 0.973)),  # init state: z=0.9723
+            ("Not", ("inair", "akita_black_bowl_1", 0.899)),  # init state: z=0.8984
         ],
     )
     bddl, _ = generate_bddl_from_task_info()
