bc8ccdcffc82243eb24caea03566f87cf4c77fee
Lakindu <lakindujay@gmail.com>
2025-06-05 11:19:08 +0800

    3 tasks
    - place the bowl in the center of the plate
    - Place the bowl on the top of the white cabinet with all drawers closed
    - place the piece of chocolate into the porcelain mug ensuring that it stands upright

---
+++ tasks/KITCHEN_SCENE5_place_the_bowl_on_the_top_of_the_white_cabinet_with_all_drawers_closed.py
@@ -0,0 +1,38 @@
+"""This is a standalone file for create a task in libero."""
+import numpy as np
+
+from libero.libero.utils.bddl_generation_utils import (
+    get_xy_region_kwargs_list_from_regions_info,
+)
+from libero.libero.utils.mu_utils import register_mu, InitialSceneTemplates
+from libero.libero.utils.task_generation_utils import (
+    register_task_info,
+    get_task_info,
+    generate_bddl_from_task_info,
+)
+
+from libero.libero.benchmark.mu_creation import *
+
+def main():
+
+    scene_name = "kitchen_scene5"
+    language = "Place the bowl on the top of the white cabinet with all drawers closed"
+    register_task_info(
+        language,
+        scene_name=scene_name,
+        objects_of_interest=["akita_black_bowl_1", "white_cabinet_1"],
+        goal_states=[
+            ("Centre", "akita_black_bowl_1", "white_cabinet_1", 0.10, 0.05, 0, 0),
+            ("Close", "white_cabinet_1_middle_region"),
+            ("Close", "white_cabinet_1_bottom_region"),
+            ("Close", "white_cabinet_1_top_region"),
+            ("Upright", "akita_black_bowl_1"),
+        ]
+    )
+
+    bddl_file_names, failures = generate_bddl_from_task_info()
+    print(bddl_file_names)
+
+
+if __name__ == "__main__":
+    main()
