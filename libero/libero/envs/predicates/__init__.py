793dc391d2d83e759f67622773f1c10391642d33
Liu Yitao <ltao@connect.hku.hk>
2025-06-05 17:48:26 +0800

    New task

--- libero/libero/envs/predicates/__init__.py
+++ libero/libero/envs/predicates/__init__.py
@@ -36,6 +36,8 @@ VALIDATE_PREDICATE_FN_DICT = {
     "above": Above(),
     "between": MidBetween(),
     "relaxedbetween": RelaxedMidBetween(),
+    "distancebetween": DistanceBetween(),
+    "flexibleon": FlexibleOn(),
 }
 
 
