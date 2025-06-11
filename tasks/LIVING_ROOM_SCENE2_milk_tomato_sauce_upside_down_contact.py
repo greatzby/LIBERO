3e3aaee2a2a44c99526d727c6404dce4a2151d1f
adlsdztony <zzl0712@connect.hku.hk>
2025-06-11 21:11:05 +0800

    ai task: 13 new tasks

---
+++ tasks/LIVING_ROOM_SCENE2_milk_tomato_sauce_upside_down_contact.py
@@ -0,0 +1,36 @@
+"""This is a standalone file for create a task in libero."""
+import numpy as np
+
+from libero.libero.utils.bddl_generation_utils import (
+    get_xy_region_kwargs_list_from_regions_info,
+)
+from libero.libero.utils.mu_utils import register_mu, InitialSceneTemplates
+from libero.libero.utils.task_generation_utils import (
+    register_task_info,
+    get_task_info,
+    generate_bddl_from_task_info,
+)
+
+from libero.libero.benchmark.mu_creation import *
+
+def main():
+
+    scene_name = "living_room_scene2"
+    language = "Turn the milk and tomato sauce upside down while keeping them next to each other and on the table"
+    
+    register_task_info(
+        language,
+        scene_name=scene_name,
+        objects_of_interest=["milk_1", "tomato_sauce_1"],
+        goal_states=[
+            ("UpsideDown", "milk_1"),
+            ("UpsideDown", "tomato_sauce_1"),
+            ("InContact", "milk_1", "tomato_sauce_1"),
+        ]
+    )
+
+    bddl_file_names, failures = generate_bddl_from_task_info()
+    print(bddl_file_names)
+
+if __name__ == "__main__":
+    main()
---
+++ tasks/LIVING_ROOM_SCENE2_tomato_sauce_between_butter_cream_cheese.py
@@ -0,0 +1,34 @@
+"""This is a standalone file for create a task in libero."""
+import numpy as np
+
+from libero.libero.utils.bddl_generation_utils import (
+    get_xy_region_kwargs_list_from_regions_info,
+)
+from libero.libero.utils.mu_utils import register_mu, InitialSceneTemplates
+from libero.libero.utils.task_generation_utils import (
+    register_task_info,
+    get_task_info,
+    generate_bddl_from_task_info,
+)
+
+from libero.libero.benchmark.mu_creation import *
+
+def main():
+
+    scene_name = "living_room_scene2"
+    language = "Position the tomato sauce on the table between the butter and cream cheese and stay in contact with both"
+    
+    register_task_info(
+        language,
+        scene_name=scene_name,
+        objects_of_interest=["tomato_sauce_1", "butter_1", "cream_cheese_1"],
+        goal_states=[
+            ("MidBetween", "butter_1", "tomato_sauce_1", "cream_cheese_1", "y"),
+        ]
+    )
+
+    bddl_file_names, failures = generate_bddl_from_task_info()
+    print(bddl_file_names)
+
+if __name__ == "__main__":
+    main()
