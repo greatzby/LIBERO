906d70179f802221bb5642756e498c6d1d3c8fda
shaoyu <zshaoyu@connect.hku.hk>
2025-06-21 18:50:34 +0800

    Fix a reward hacking

--- tasks/LIVING_ROOM_SCENE3_lay_all_items_flat_in_the_tray_and_ensure_them_touching_the_bottom_of_the_tray.py
+++ tasks/LIVING_ROOM_SCENE3_lay_all_items_flat_in_the_tray_and_ensure_them_touching_the_bottom_of_the_tray.py
@@ -15,6 +15,20 @@ from libero.libero.benchmark.mu_creation import *
 def main():
     scene_name = "living_room_scene3"
     language = "Lay all items flat in the tray and ensure them touching the bottom of the tray"
+    # wooden_tray_z = 0.43738009
+    # laid_butter_z = 0.45281869
+    # laid_ketchup_z = 0.46391021
+    # laid_tomato_sauce_z = 0.47818113
+    # butter_height = laid_butter_z - wooden_tray_z
+    # ketchup_height = laid_ketchup_z - wooden_tray_z
+    # tomato_sauce_height = laid_tomato_sauce_z - wooden_tray_z
+    # tolerance = 0.005
+    # butter_min_z = butter_height - tolerance
+    # butter_max_z = butter_height + tolerance
+    # ketchup_min_z = ketchup_height - tolerance
+    # ketchup_max_z = ketchup_height + tolerance
+    # tomato_sauce_min_z = tomato_sauce_height - tolerance
+    # tomato_sauce_max_z = tomato_sauce_height + tolerance
     register_task_info(
         language,
         scene_name=scene_name,
@@ -25,11 +39,11 @@ def main():
             ("RelaxedOn", "tomato_sauce_1", "wooden_tray_1"),
             ("RelaxedOn", "ketchup_1", "wooden_tray_1"),
             ("RelaxedOn", "butter_1", "wooden_tray_1"),
-            ("InContact", "alphabet_soup_1", "wooden_tray_1"),
-            ("InContact", "cream_cheese_1", "wooden_tray_1"),
-            ("InContact", "tomato_sauce_1", "wooden_tray_1"),
-            ("InContact", "ketchup_1", "wooden_tray_1"),
-            ("InContact", "butter_1", "wooden_tray_1"),
+            ("PositionWithinObject", "alphabet_soup_1", "wooden_tray_1", -0.1, -0.1, 0.035801, 0.1, 0.1, 0.045801),
+            ("PositionWithinObject", "tomato_sauce_1", "wooden_tray_1", -0.1, -0.1, 0.035801, 0.1, 0.1, 0.045801),
+            ("PositionWithinObject", "butter_1", "wooden_tray_1", -0.1, -0.1, 0.010438, 0.1, 0.1, 0.020438),
+            ("PositionWithinObject", "cream_cheese_1", "wooden_tray_1", -0.1, -0.1, 0.010438, 0.1, 0.1, 0.020438),
+            ("PositionWithinObject", "ketchup_1", "wooden_tray_1", -0.1, -0.1, 0.02153011, 0.1, 0.1, 0.03153011),
             ("AxisAlignedWithinWorldAxis", "alphabet_soup_1", "y", 85, 95, "z"),
             ("AxisAlignedWithinWorldAxis", "tomato_sauce_1", "y", 85, 95, "z"),
             ("Or", ("AxisAlignedWithinWorldAxis", "cream_cheese_1", "z", 0, 5, "z"), ("AxisAlignedWithinWorldAxis", "cream_cheese_1", "z", 175, 180, "z")),
