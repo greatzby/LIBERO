2d029e009ee996d00d89b49146463ca6998602b6
by zheng <u3612899@connect.hku.hk>
2025-06-23 20:19:17 +0800

    Form a straight line using the three mugs and make sure their handles aligned

---
+++ tasks/LIVING_ROOM_SCENE5_form_a_straight_line_using_the_three_mugs_and_make_sure_their_handles_aligned.py
@@ -0,0 +1,42 @@
+"""This is a standalone file for create a task in libero."""
+from libero.libero.utils.bddl_generation_utils import (
+    get_xy_region_kwargs_list_from_regions_info,
+)
+from libero.libero.utils.mu_utils import register_mu, InitialSceneTemplates
+from libero.libero.utils.task_generation_utils import (
+    register_task_info,
+    generate_bddl_from_task_info,
+)
+import numpy as np
+
+from libero.libero.benchmark.mu_creation import LivingRoomScene5
+
+def main():
+    scene_name = "living_room_scene5"
+    language = "Form a straight line using the three mugs and make sure their handles aligned"
+
+    register_task_info(
+        language,
+        scene_name=scene_name,
+        objects_of_interest=["porcelain_mug_1","red_coffee_mug_1","white_yellow_mug_1"],
+        goal_states=[
+            
+            ("linear",
+                "porcelain_mug_1",
+                "red_coffee_mug_1",
+                "white_yellow_mug_1",
+                0.005,
+            ),
+
+            ("yawanglealigned", "porcelain_mug_1", "red_coffee_mug_1",   15.0, 180.0),
+            ("yawanglealigned", "porcelain_mug_1", "white_yellow_mug_1", 15.0, 180.0),    
+                
+                
+        ],
+    )
+
+    bddl_file_names, failures = generate_bddl_from_task_info()
+    print(bddl_file_names)
+
+if __name__ == "__main__":
+    main()
